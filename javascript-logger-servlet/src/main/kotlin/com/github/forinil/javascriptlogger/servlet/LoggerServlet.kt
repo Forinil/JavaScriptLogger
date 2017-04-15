6f42af7294f84604116d82d569aa5990533a50b6
Konrad Botor <kbotor@gmail.com>
2017-04-15 16:08:40 +0200

    Initial commit
    Initial commit

---
+++ javascript-logger-servlet/src/main/kotlin/com/github/forinil/javascriptlogger/servlet/LoggerServlet.kt
@@ -0,0 +1,27 @@
+package com.github.forinil.javascriptlogger.servlet
+
+import org.slf4j.LoggerFactory
+import java.io.IOException
+import javax.servlet.ServletException
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+/**
+ * Created by Konrad Botor on 15.04.2017T15:42
+ * in package com.github.forinil.javascriptlogger.servlet in project JavaScriptLogger.
+ */
+@WebServlet(name = "LoggerServlet",
+        displayName = "LoggerServlet",
+        description = "Logger Servlet",
+        loadOnStartup = 1,
+        urlPatterns = arrayOf("/logger/*"))
+open class LoggerServlet: HttpServlet() {
+    private val logger = LoggerFactory.getLogger(LoggerServlet::class.java)
+
+    @Throws(ServletException::class, IOException::class)
+    protected fun LoggerServlet.doPost(req: HttpServletRequest, resp: HttpServletResponse) {
+        logger.trace("doPost")
+    }
+}
---
+++ javascript-logger-webjar/src/main/kotlin/com/github/forinil/javascriptlogger/webjar/Logger.kt
@@ -0,0 +1,19 @@
+package com.github.forinil.javascriptlogger.webjar
+
+/**
+* Created by Konrad Botor on 15.04.2017T15:40
+* in package com.github.forinil.javascriptlogger.webjar in project JavaScriptLogger.
+*/
+object Logger {
+    fun info() {
+        print("test")
+    }
+
+    fun debug() {
+
+    }
+
+    fun error() {
+
+    }
+}
