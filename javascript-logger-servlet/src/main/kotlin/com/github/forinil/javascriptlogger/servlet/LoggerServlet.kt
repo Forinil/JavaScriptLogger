a136ed8f2a98dd960bf2d3e3f3517dae25159dcb
Konrad Botor <kbotor@gmail.com>
2017-04-17 16:41:14 +0200

    :sparkles: Added logging data based on received request
    :sparkles: Added logging data based on received request

--- javascript-logger-servlet/src/main/kotlin/com/github/forinil/javascriptlogger/servlet/LoggerServlet.kt
+++ javascript-logger-servlet/src/main/kotlin/com/github/forinil/javascriptlogger/servlet/LoggerServlet.kt
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.forinil.javascriptlogger.servlet.util.LogData
+import com.github.forinil.javascriptlogger.servlet.util.ResponseData
+import com.github.forinil.javascriptlogger.servlet.util.ResponseStatus
 import org.slf4j.event.Level
 
 
@@ -23,18 +25,47 @@ import org.slf4j.event.Level
         urlPatterns = arrayOf("/logger"))
 open class LoggerServlet: HttpServlet() {
     private val logger = LoggerFactory.getLogger(LoggerServlet::class.java)
+    private val mapper = ObjectMapper()
+    private val messageTemplate = "{}::{} - {}"
+    private val errorTemplate = messageTemplate + " - {}"
 
     @Throws(ServletException::class, IOException::class)
     override fun doPost(req: HttpServletRequest, resp: HttpServletResponse) {
         logger.trace("doPost")
+
         // Parse request
-        val mapper = ObjectMapper()
-        mapper.readValue(req.reader, LogData::class.java)
+        val request = mapper.readValue(req.reader, LogData::class.java)
+        val level: Level
+        try {
+            level = getLevel(request)
+            logger.trace("Data received: {}", request)
+        } catch(e: Exception) {
+            logger.error("Unknown log level", e)
+            sendResponse(resp, ResponseData(ResponseStatus.FAILURE, "Unknown log level"))
+            return
+        }
+
+        when (level) {
+            Level.INFO -> logger.info(messageTemplate, request.page, request.function, request.message)
+            Level.TRACE -> logger.trace(messageTemplate, request.page, request.function, request.message)
+            Level.DEBUG -> logger.debug(messageTemplate, request.page, request.function, request.message)
+            Level.WARN -> logger.warn(messageTemplate, request.page, request.function, request.message)
+            Level.ERROR -> logger.error(errorTemplate, request.page, request.function, request.message, request.errorCode)
+            else -> {
+                logger.error("Unknown log level in request: {}", request)
+                sendResponse(resp, ResponseData(ResponseStatus.FAILURE, "Unknown log level"))
+                return
+            }
+        }
 
         // Send response
+        sendResponse(resp, ResponseData())
+    }
+
+    private fun sendResponse(resp: HttpServletResponse, resonseData: ResponseData) {
         resp.contentType = "text/html"
         val out = resp.writer
-        out.println("{\"result\": \"OK\"}")
+        out.println(mapper.writeValueAsString(resonseData))
     }
 
     private fun getLevel(logData: LogData): Level {
--- javascript-logger-servlet/src/test/kotlin/com/github/forinil/javascriptlogger/servlet/LoggerServletTest.kt
+++ javascript-logger-servlet/src/test/kotlin/com/github/forinil/javascriptlogger/servlet/LoggerServletTest.kt
@@ -2,13 +2,13 @@ package com.github.forinil.javascriptlogger.servlet
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.forinil.javascriptlogger.servlet.util.LogData
+import com.github.forinil.javascriptlogger.servlet.util.ResponseData
 import org.junit.Test
 import org.mockito.Mockito
 import org.slf4j.LoggerFactory
-import java.io.BufferedReader
-import java.io.ByteArrayOutputStream
-import java.io.PrintWriter
-import java.io.StringReader
+import org.slf4j.impl.SimpleLogger
+import java.io.*
+import java.util.*
 import javax.servlet.http.HttpServletResponse
 import javax.servlet.http.HttpServletRequest
 import kotlin.test.junit.JUnitAsserter
@@ -20,6 +20,7 @@ import kotlin.test.junit.JUnitAsserter
  */
 class LoggerServletTest: Mockito() {
     private val logger = LoggerFactory.getLogger(LoggerServletTest::class.java)
+    private val mapper = ObjectMapper()
 
     @Test
     fun testServlet() {
@@ -27,8 +28,7 @@ class LoggerServletTest: Mockito() {
         val response = Mockito.mock(HttpServletResponse::class.java)
         val output = ByteArrayOutputStream()
         val writer = PrintWriter(output)
-        val logData = LogData("INFO", "test", "mock", "mock test", null)
-        val mapper = ObjectMapper()
+        val logData = LogData("INFO", "mockPage", "mockFunction", "mockMessage", null)
         val input = mapper.writeValueAsString(logData)
         val reader = BufferedReader(StringReader(input))
 
@@ -42,10 +42,19 @@ class LoggerServletTest: Mockito() {
 
         writer.flush()
 
+        // Test response
         val result = output.toString("UTF-8").replace("\n", "").replace("\r", "")
-        val expected = "{\"result\": \"OK\"}"
+        val expected = mapper.writeValueAsString(ResponseData())
 
-        logger.info("Actual: {}, expected: {}", result, expected)
-        JUnitAsserter.assertTrue("Wrong output", result.contains(expected))
+        logger.info("Actual response: {}, expected response: {}", result, expected)
+        JUnitAsserter.assertTrue("Wrong response", result.contains(expected))
+
+        // Test contents of the log file
+        val loggerConfiguration = Properties()
+        loggerConfiguration.load(this::class.java.getResourceAsStream("/simplelogger.properties"))
+        val logFile = FileReader(loggerConfiguration.getProperty(SimpleLogger.LOG_FILE_KEY))
+        val logFileContents = logFile.readText()
+
+        JUnitAsserter.assertTrue("Incorrect log file content", logFileContents.contains("mockPage::mockFunction - mockMessage"))
     }
 }
