eed8eecd09445a3fd271fcb497a66fb9bf2e45fc
Konrad Botor <kbotor@gmail.com>
2017-04-19 17:07:06 +0200

    :sparkles: Finished test app
    :sparkles: Finished test app

--- javascript-logger-testapp/src/main/kotlin/com/github/forinil/javascriptlogger/testapp/TestServlet.kt
+++ javascript-logger-testapp/src/main/kotlin/com/github/forinil/javascriptlogger/testapp/TestServlet.kt
@@ -33,28 +33,119 @@ open class TestServlet: HttpServlet() {
 		logger.info("doGet")
 
 		//Generate webpage
-		val webPage = StringBuilder().appendHTML(true).html {
+		val webPage = generateWebPage()
+
+		// Send response
+		resp.contentType = "text/html"
+		val out = resp.writer
+		out.println(webPage)
+	}
+
+	private fun generateWebPage(): String {
+		return StringBuilder().appendHTML(true).html {
 			head {
 				title {
 					+"JavaScript Logger Test Application"
 				}
+				link("webjars/bootstrap/3.3.7-1/css/bootstrap.min.css", "stylesheet")
 				script(ScriptType.textJavaScript, "webjars/javascript-logger-webjar/1.0-SNAPSHOT/js/lib/kotlin.js")
 				script(ScriptType.textJavaScript, "webjars/javascript-logger-webjar/1.0-SNAPSHOT/js/javascript-logger-webjar.js")
 				script(ScriptType.textJavaScript, "static/js/logger.js")
 			}
 			body {
 				div {
+					classes = setOf("container")
 					span {
+						style = "display: none"
 						onClick = "info()"
 						+"INFO test"
 					}
+					div {
+						classes = setOf("panel", "panel-default")
+						div {
+							classes = setOf("panel-body")
+							form {
+								action = ""
+								id = "loggerTestForm"
+								classes = setOf("form-horizontal")
+								div {
+									classes = setOf("form-group")
+									label {
+										for_ = "messageText"
+										classes = setOf("col-sm-2", "control-label")
+										+"Message"
+									}
+									div {
+										classes = setOf("col-sm-10")
+										input {
+											type = InputType.text
+											id = "messageText"
+											classes = setOf("form-control")
+										}
+									}
+								}
+								div {
+									classes = setOf("form-group")
+									label {
+										for_ = "errorCode"
+										classes = setOf("col-sm-2", "control-label")
+										+"Error code"
+									}
+									div {
+										classes = setOf("col-sm-10")
+										input {
+											type = InputType.text
+											id = "errorCode"
+											classes = setOf("form-control")
+										}
+									}
+								}
+								div {
+									classes = setOf("form-group")
+									div {
+										classes = setOf("col-sm-offset-2", "col-sm-10")
+										button {
+											type = ButtonType.button
+											classes = setOf("btn", "btn-info")
+											style = "margin: 5px; padding: 6px 12px;"
+											onClick = "info()"
+											+"INFO"
+										}
+										button {
+											type = ButtonType.button
+											classes = setOf("btn", "btn-primary")
+											style = "margin: 5px; adding: 6px 12px;"
+											onClick = "debug()"
+											+"DEBUG"
+										}
+										button {
+											type = ButtonType.button
+											classes = setOf("btn", "btn-warning")
+											style = "margin: 5px; padding: 6px 12px;"
+											onClick = "warn()"
+											+"WARN"
+										}
+										button {
+											type = ButtonType.button
+											classes = setOf("btn", "btn-default")
+											style = "margin: 5px; padding: 6px 12px;"
+											onClick = "trace()"
+											+"TRACE"
+										}
+										button {
+											type = ButtonType.button
+											classes = setOf("btn", "btn-danger")
+											style = "margin: 5px; padding: 6px 12px;"
+											onClick = "error()"
+											+"ERROR"
+										}
+									}
+								}
+							}
+						}
+					}
 				}
 			}
-		}
-
-		// Send response
-		resp.contentType = "text/html"
-		val out = resp.writer
-		out.println(webPage.toString())
+		}.toString()
 	}
 }
--- javascript-logger-testapp/src/main/webapp/static/js/logger.js
+++ javascript-logger-testapp/src/main/webapp/static/js/logger.js
@@ -1,5 +1,50 @@
 var logger = this["javascript-logger-webjar"].com.github.forinil.javascriptlogger.webjar.Logger
 
 function info() {
-	logger.info('index', 'span_click', 'INFO level test')
+	var message = document.getElementById('messageText').value;
+	logger.info('index', 'info_test', message)
 }
+
+function debug() {
+	var message = document.getElementById('messageText').value;
+	logger.debug('index', 'debug_test', message)
+}
+
+function warn() {
+	var message = document.getElementById('messageText').value;
+	logger.warn('index', 'warn_test', message)
+}
+
+function trace() {
+	var message = document.getElementById('messageText').value;
+	logger.trace('index', 'trace_test', message)
+}
+
+function error() {
+	var message = document.getElementById('messageText').value;
+	var errorCode = document.getElementById('errorCode').value;
+	if (errorCode === '') {
+		errorCode = null
+	}
+	logger.error('index', 'error_test', message, errorCode)
+}
+
+window.onerror = function (msg, url, lineNo, columnNo, error) {
+    var string = msg.toLowerCase();
+    var substring = "script error";
+    if (string.indexOf(substring) > -1){
+        logger.error('index', 'onError', 'Script Error: See Browser Console for Detail');
+    } else {
+        var message = [
+            'Message: ' + msg,
+            'URL: ' + url,
+            'Line: ' + lineNo,
+            'Column: ' + columnNo,
+            'Error object: ' + JSON.stringify(error)
+        ].join(' - ');
+
+        logger.error('index', 'onError', message, -1);
+    }
+
+    return false;
+};
